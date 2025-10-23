52a386ca48afbbc812043fa2d348de5e83726319
opilane <opilane@LENOVO-PF260C6A>
2025-10-23 11:11:41 +0300

    Registration Model & ASPNET Identity

---
+++ Chatter.Server/Models/UserRegistrationModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chatter.Server.Models
+{
+    public class UserRegistrationModel
+    {
+        [Required(ErrorMessage = "Username is invalid or taken.")]
+        public string UserName {  get; set; }
+
+        [Required(ErrorMessage = "Email is invalid or taken.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is invalid.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPasswword { get; set; }
+    }
+}
