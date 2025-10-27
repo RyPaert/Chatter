34dfa1356041896b5bfd149dd2edfee10a6178e3
Ry <thomashenrypaert@gmail.com>
2025-10-27 17:51:39 +0200

    message: get, post, delete
    added api endpoints for getting, posting and deleting messages.
    
    https://trello.com/c/qSefpjgX/9-as-a-developer-i-want-the-user-to-be-able-to-send-messages-so-they-can-interact-with-other-users

---
+++ Chatter.Server/Controllers/MessagesController.cs
@@ -0,0 +1,39 @@
+﻿using Chatter.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Chatter.Server.Controllers
+{
+	[ApiController]
+	[Route("messages")]
+	public class MessagesController : ControllerBase
+	{
+		private static readonly List<Message> Messages = new List<Message>();
+
+		[HttpGet]
+		public ActionResult<IEnumerable<Message>> GetMessages()
+		{
+			return Ok(Messages);
+		}
+
+		[HttpPost]
+		public ActionResult<Message> PostMessages([FromBody] Message message)
+		{
+			if (message == null) return BadRequest();
+
+			message.Id = Guid.NewGuid();
+			Messages.Add(message);
+			return Ok(message);
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(Guid id)
+		{
+			var message = Messages.FirstOrDefault(m => m.Id == id);
+			if (message == null) return NotFound();
+
+			Messages.Remove(message);
+			return NoContent();
+		}
+	}
+}
