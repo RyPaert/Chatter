1726363ee0e890a72b42601c36be0c29663aba19
Opilane TTHK <opilane@tthk.ee>
2025-10-30 11:50:04 +0200

    foundation for accounts
    Added a User model and an accountsController with a WIP setUsername function

---
+++ Chatter.Server/Controllers/AccountsController.cs
@@ -0,0 +1,22 @@
+﻿using Chatter.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chatter.Server.Controllers
+{
+    [ApiController]
+    [Route("accounts")]
+    public class AccountsController : ControllerBase
+    {
+        [HttpPost("{username}")]
+        public IActionResult setUsername(User user, string username)
+        {
+            if (username == null || string.IsNullOrWhiteSpace(username)) { return BadRequest(); }
+            else if (ModelState.IsValid)
+            {
+                user.Id = new Guid();
+                user.Name = username;
+            }
+            return Ok(username);
+        }
+    }
+}
---
+++ Chatter.Server/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Chatter.Server.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
