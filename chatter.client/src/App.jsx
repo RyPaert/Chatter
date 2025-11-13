252cda35b0a4570aac3bfd6924cd1debdbaeb782
opilane <opilane@HP-CZC425727V>
2025-11-13 10:44:59 +0200

    butten for the user

--- chatter.client/src/App.jsx
+++ chatter.client/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 export default function App() {
     return (
         <div className="App">
+        <button className="button_for_name">User name</button>
             <h1 className="chat-bigTitle">Live Chat</h1>
             <Chat />
         </div>
